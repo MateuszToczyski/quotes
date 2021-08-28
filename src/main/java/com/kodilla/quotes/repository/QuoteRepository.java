51e81a299c28fa45bc75b2997b28def96945081b
Mateusz Toczyski <toczyski.mateusz@gmail.com>
2021-08-28 21:15:20 +0200

    Init

---
+++ src/main/java/com/kodilla/quotes/repository/QuoteRepository.java
@@ -0,0 +1,7 @@
+package com.kodilla.quotes.repository;
+
+import com.kodilla.quotes.model.Quote;
+import org.springframework.data.repository.CrudRepository;
+
+public interface QuoteRepository extends CrudRepository<Quote, Long> {
+}
